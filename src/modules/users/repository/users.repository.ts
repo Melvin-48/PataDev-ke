fbc1c17c8ebb13ca9a64a4fc502029de9da661ae
Lawrence Kimani <larrykimani01@gmail.com>
2026-08-24 19:12:39 +0300

    Some messaging work

--- src/modules/users/repository/users.repository.ts
+++ src/modules/users/repository/users.repository.ts
@@ -6,10 +6,34 @@ export class UsersRepository {
   constructor(private prisma: PrismaService) {}
 
   findById(id: string) {
-    return this.prisma.user.findUnique({ where: { id } });
+    return this.prisma.user.findUnique({
+      where: { id },
+      include: {
+        clientProfile: true,
+        developerProfile: true,
+      },
+    });
   }
 
   findBySupabaseId(supabaseId: string) {
     return this.prisma.user.findUnique({ where: { supabaseId } });
+  }
+
+  createClientProfile(userId: string, data: any) {
+    return this.prisma.clientProfile.create({
+      data: {
+        ...data,
+        userId,
+      },
+    });
+  }
+
+  createDeveloperProfile(userId: string, data: any) {
+    return this.prisma.developerProfile.create({
+      data: {
+        ...data,
+        userId,
+      },
+    });
   }
 }
